6ca7bd7aba3cfd43f7e4f9d2af40e2bfa2e670ed
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-10 13:17:03 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -6,12 +6,11 @@ export default class CoordinatesButton extends Component {
   
   handleClick = event => {
     this.props.onReceiveCoordinates([event.clientX, event.clientY])
-  }
+  };
 
   render(){
     return (
       <button onClick={this.handleClick}></button>
-    )
-  }
-
-}
+    );
+  };
+};
